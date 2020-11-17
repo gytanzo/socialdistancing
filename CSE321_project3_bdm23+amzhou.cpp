010cfceee1208bacf3eb3057964160e77375d305
gytanzo <gytanzo@gmail.com>
2020-11-17 16:40:18 -0500

    Moved port for LCD; properly kick Watchdog

--- CSE321_project3_bdm23+amzhou.cpp
+++ CSE321_project3_bdm23+amzhou.cpp
@@ -47,17 +47,19 @@ int main()
     // Use Port B for inputs
     GPIOB->MODER &= ~(0x000F0000);          // Set 0s for 8/9
     // Use Port C for outputs
-    GPIOC->MODER &= ~(0x00AA0000);          // Set 0s for Registers 8/9/10/11
-    GPIOC->MODER |= 0x00550000;             // Set 1s for Registers 8/9/10/11    
+    GPIOC->MODER &= ~(0x00AA000A);          // Set 0s for Registers 8/9/10/11
+    GPIOC->MODER |= 0x00550005;             // Set 1s for Registers 8/9/10/11    
 
-    GPIOC -> ODR |= 0x100;
     while (true) {
         if (check(8) == 0x0){               // Sound detected, turn everything on 
             watch.start(wdTimeout);         // Start the watchdog
+            GPIOC -> ODR |= 0x1;            // Turn on LCD
             lcd.begin();
             lcd.print("You may walk.");     // First person gets to walk
             while(true){                    // After everything has been turned on
-                ;
+                if (check(8) == 0x0){
+                    watch.kick();
+                }
             }
         }
         else;                               // Sound not detected, do nothing 
