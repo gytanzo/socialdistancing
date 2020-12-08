e232ed15110240bfb1c8d59713793772443f7553
gytanzo <gytanzo@gmail.com>
2020-12-08 15:46:10 -0500

    Seven segment counts up once a second

--- CSE321_project3_bdm23+amzhou.cpp
+++ CSE321_project3_bdm23+amzhou.cpp
@@ -47,25 +47,28 @@ void lcdgo();                                   // Function to print to LCD to g
 // set up LCD
 CSE321_LCD lcd(16,2,LCD_5x8DOTS,PF_0,PF_1);     // PF0 = SDA, PF1 = SCL
 Ultrasonic ultrasonic(PC_10, PB_9);             // PC_10 is TP, PB_9 is EP; these values are not used but rather included for transparency and ease of understanding
-DigitDisplay dd(PC_4, PC_5);                    // dd is digital display; PC_8 = CLK, PC_9 = DIO
+DigitDisplay dd(PC_4, PC_5);                    // dd is digital display; PC_4 = CLK, PC_5 = DIO
+
+Ticker tick;                                    // Ticker to run counting function
+int timeLeft;                                   // To keep track of the time left
 
 int main()
 {
     dd.on();                                    // Initialize 7-Seg Display
     dd.clear();                                 // Clear anything leftover 
-    dd.write(46);
-
     RCC->AHB2ENR |= 0x6;                        // Enable Clock for GPIOC and GPIOB
     // Use Port B for inputs
     GPIOB->MODER &= ~(0xF0000);                 // Set 0s for 8/9
     // Use Port C for outputs
-    GPIOC->MODER &= ~(0x20000A);                // Set 0s for Registers 0/1/2/10
-    GPIOC->MODER |= 0x100005;                   // Set 1s for Registers 0/1/2/10    
+    GPIOC->MODER &= ~(0x20002A);                // Set 0s for Registers 0/1/2/10
+    GPIOC->MODER |= 0x100015;                   // Set 1s for Registers 0/1/2/10   
+
+    tick.attach(&startCount, 1);
 
     while (true) {
         if (check(8) == 0x0){                   // Sound detected; turn everything on 
             watch.start(wdTimeout);             // Start watchdog
-            GPIOC -> ODR |= 0x3;                // Turn on LCD, ultrasonic transducer, (and later) seven segment display
+            GPIOC -> ODR |= 0x7;                // Turn on LCD, ultrasonic transducer, (and later) seven segment display
 
             lcd.begin();                        // Initialize LCD
             lcd.print("You may walk.");         // First person gets to walk
@@ -75,7 +78,7 @@ int main()
             while (base > 156) {                // Edge case where distance jumpts up to 475 in
                 base = ultrasonic.Ranging(INC); // Re-adjust base value
             } 
-            while(true){     
+            while(true){    
                 long distance = ultrasonic.Ranging(INC);
                 if (distance <= 156){           // Edge case where distance jumps up to 475 in
                     long difference = base - distance;
@@ -108,7 +111,8 @@ int check(int bit){                             // Returns the value of a bit gi
 }
 
 void startCount(){                              // Andrew is eventually going to do something with this
-    ;
+    timeLeft += 1;
+    dd.write(timeLeft);
 }
 
 void lcdwait(){                                 // Tell person to kindly wait for their turn
